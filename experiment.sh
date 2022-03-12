c127d0bedc4ca3ea10a2cf67f4e4f631150bcc58
roh6608 <72960901+roh6608@users.noreply.github.com>
2022-03-12 18:49:02 +1100

    updates

--- experiment.sh
+++ experiment.sh
@@ -10,10 +10,12 @@ cd ./C && gcc mat.c -o c-mat && ./c-mat "./c-results${counter}.txt" && cd .. && 
 
 cd ./fortran && gfortran mat.f90 -o f-mat && ./f-mat "./f-results${counter}.txt" && cd .. && echo "Fortran-lang Complete"
 
+cd ./Julia && julia mat.jl "./j-results${counter}.txt" && cd .. && echo "Julia-lang Complete"
+
 cd ./R && Rscript mat.R "./r-results${counter}.txt" && cd .. && echo "R-lang Complete"
 
 cd ./Python && python3 mat.py "./p-results${counter}.txt" && cd .. && echo "Python-lang Complete"
 
-mv "./C/c-results${counter}.txt" ./results/ && mv "./fortran/f-results${counter}.txt" ./results && mv "./R/r-results${counter}.txt" ./results/ && mv "./Python/p-results${counter}.txt" ./results/
+mv "./C/c-results${counter}.txt" ./results/ && mv "./fortran/f-results${counter}.txt" ./results && mv "./R/r-results${counter}.txt" ./results/ && mv "./Python/p-results${counter}.txt" ./results/ && mv "./Julia/j-results${counter}.txt" ./results/
 done
 echo "Experiment Complete"
